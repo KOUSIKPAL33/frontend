ea2263d13c9c460ef8ecb890f89e91c05048cc04
kousik Pal <kousikpal652@gmail.com>
2025-07-14 02:38:18 +0530

    multiple update

--- src/Url.jsx
+++ src/Url.jsx
@@ -1,3 +1,3 @@
-//const baseurl = "https://backend-3qq4.onrender.com/api";
- const baseurl = "http://localhost:5000/api"; // Uncomment for local development
+const baseurl = "https://backend-3qq4.onrender.com/api";
+//const baseurl = "http://localhost:5000/api"; // Uncomment for local development
 export default baseurl;
